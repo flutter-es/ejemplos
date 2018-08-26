6cd4f0c0e6a4d044d208d190c9414567e921544d
Daniel Aguilera <twinber@hotmail.com>
2018-08-27 01:27:01 +0200

    modificado travis_script

--- travis_script.sh
+++ travis_script.sh
@@ -6,16 +6,6 @@ set -e
 # Normalize Branch variable
 export BRANCH=$(if [ "$TRAVIS_PULL_REQUEST" == "false" ]; then echo $TRAVIS_BRANCH; else echo $TRAVIS_PULL_REQUEST_BRANCH; fi)
 
-# Use the version of Dart SDK from the Flutter repository instead of whatever
-# version is in the PATH.
-export PATH="../flutter/bin/cache/dart-sdk/bin:$PATH"
-
-echo "Path to dart is:"
-which dart
-
-echo "Using Dart version:"
-dart --version
-
 # Analyze cookbook samples
 
 for D in cookbook/*; do
