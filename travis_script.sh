d5393867efb2ab809a895ab05a60a60162a83650
Daniel Aguilera <twinber@hotmail.com>
2018-08-27 03:01:00 +0200

    correcion rutas

--- travis_script.sh
+++ travis_script.sh
@@ -30,10 +30,10 @@ for D in *; do
   if [ -d "${D}" ]; then
     cd ${D}
     echo "Run flutter packages get on ${D}"
-    ../flutter/bin/flutter packages get ../../${D}
+    ../flutter/bin/flutter packages get 
 		
 		echo "Run flutter analyze on ${D}"
-		../flutter/bin/flutter analyze ../../${D}
+		../flutter/bin/flutter analyze 
     cd .. 
 
   fi
